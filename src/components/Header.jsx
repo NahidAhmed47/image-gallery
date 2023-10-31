096c0ff406a77716276322adab3e35dea1bc2a94
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-10-31 19:13:59 +0600

    header added

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='px-3 sm:px-4 md:px-5 lg:px-8 xl:px-10 py-3 md:py-4 border-b-2'>
+            <h1 className='text-lg md:text-xl lg:text-2xl font-bold text-gray-700'>Gallery</h1>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/pages/Gallery.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../components/Header";
+
+const Gallery = () => {
+  return (
+    <div className="md:px-5 w-full h-full">
+      <div className="max-w-[1280px] mx-auto md:my-5 xl:my-10 md:border min-h-[70vh] rounded-lg">
+        <Header />
+        {/* Gallery body start from here */}
+        <div className="px-3 sm:px-4 md:px-5 lg:px-8 xl:px-10 py-3 md:py-4">
+            
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;
