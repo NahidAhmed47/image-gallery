08ef4e57bc2d8eb44228c7b59834786452941c8b
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-10-31 18:53:04 +0600

    gallery component created

---
+++ src/Gallery.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Gallery = () => {
+    return (
+        <div className='font-extrabold text-5xl text-center'>
+            this is gallery
+        </div>
+    );
+};
+
+export default Gallery;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import Gallery from './gallery'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Gallery/>
+  </React.StrictMode>
 )
